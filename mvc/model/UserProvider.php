a7d36ae111ba9d607f67815a939e85edc2171708
Дарья <yppix@yandex.ru>
2022-12-28 16:23:41 +0300

    add files

---
+++ mvc/model/UserProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+class UserProvider
+{
+    private $accounts = [
+        'admin' => '123',
+    ];
+
+    public function getByUsernameAndPassword(string $username, string $password): ?User
+    {
+        $expectedPassword = $this->accounts[$username] ?? null;
+        if ($expectedPassword === $password) {
+            return new User($username);
+        }
+
+        return null;
+    }
+}
